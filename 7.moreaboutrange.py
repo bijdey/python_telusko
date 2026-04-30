987d12b2cae0d33693e3e4a0ee0139fb24b06514
bijdey <creativenetwork.bijay@gmail.com>
2026-04-30 13:30:27 +0530

    completed file 7

--- 7.moreaboutrange.py
+++ 7.moreaboutrange.py
@@ -5,4 +5,9 @@ s={4, 5, 6}
 r= range(10) ## or we can also write r= range(0, 10)
 print(set(r))
 
+##what if i want to print the seque with a gap, 
+print(set(range(2,15,4))) #{2, 10, 6, 14}, as set is unordered
+##note here the thing is that in range(a,b,c) 
+# a is starting b is ending but not included and c is the gap
+
 
