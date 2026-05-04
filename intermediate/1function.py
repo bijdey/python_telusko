583f206bfa67c22d4c441b8bf04bb7c9b35a5cc6
bijdey <creativenetwork.bijay@gmail.com>
2026-05-04 10:54:13 +0530

    working on function file

--- intermediate/1function.py
+++ intermediate/1function.py
@@ -1,13 +1,30 @@
 
 
+## in this function we are hardcoding the values
 def add():
     a=2
     b=1
     c= a+b
     print(c)
 
+
+##calling the add function(the hardcorded function)
+add() ##this is the hardcorded function
+
+
+def addition(a,b):
+    print(a+b)
+
+##calling the addition function (dynamic function)
+addition(3,5)
+
+
+
 def sum(a, b):
     return a+b
+##printing the function like this also, here nothing will be printed if we use #sum(3,6)
+print(sum(9,8)) 
 
-add()
-print(sum(9,8))
+
+
+## an important concept, how none gets printed,
