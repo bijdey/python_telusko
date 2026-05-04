23746774ebd70c020495742e1eb42e21768bbd56
bijdey <creativenetwork.bijay@gmail.com>
2026-05-04 17:50:29 +0530

    elif

---
+++ intermediate/6elif.py
@@ -0,0 +1,17 @@
+#if, elif......, else.
+
+
+num = 3
+sal= 90
+if num == 1:
+    print('One')
+elif num == 2:
+    print('Two')
+elif num == 3:
+    print('Three')
+    if sal>= 5:
+        print('salary is greater than or equalto 5')
+else:
+     print('Number is not between 0 and 4')
+
+print('end')
