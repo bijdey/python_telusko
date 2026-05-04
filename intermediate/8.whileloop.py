fcba1bd3e997fdb9b08a856cf617fd35b1436b05
Bijay Pandey <pandey85880@gmail.com>
2026-05-04 23:28:03 +0530

    done while loop

--- intermediate/8.whileloop.py
+++ intermediate/8.whileloop.py
@@ -1,1 +1,30 @@
-## 3:38 exact
+## 3:28 exact 
+
+# while loop
+# i = 1
+# while i <= 4:
+#     print('hello world', i, sep=', ')
+#     i += 1 ## note that in python i++ does not make increment
+#     print('value of i after increment is:', i, sep=', ')
+
+
+
+
+
+#### 
+##nested
+
+i = 1
+
+while i <= 3:
+    print("Telusko", end=' ')
+    
+    j = 1
+    while j <= 5:
+        print("Rocks!!", end=' ')
+        j += 1
+    
+    i += 1
+    print(i)
+
+
