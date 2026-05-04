9fb458a45cb96a77592cb0e2a8de087543988e25
bijdey <creativenetwork.bijay@gmail.com>
2026-05-04 18:00:12 +0530

    match_switch

---
+++ intermediate/7.match_switch.py
@@ -0,0 +1,15 @@
+num = 6
+
+match num:
+    case 1:
+        print('One')
+    case 2:
+        print('Two')
+    case 3:
+        print('Three')
+    case 4:
+        print('Four')
+    case 5:
+        print('Five')
+    case _:
+        print('Number is not between 0 and 6')
