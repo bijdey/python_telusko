014675aa230f0cc41763faf43bb39b4966c92802
Bijay Pandey <bijaymac@Bijays-MacBook-Air.local>
2026-07-07 19:44:16 +0530

    added the things

--- intermediate/11array.py
+++ intermediate/11array.py
@@ -2,3 +2,9 @@
 ## 3:58
 from array import *
 
+## note that one array has only one type of data, 
+# so we cannot store different types of data in one array
+
+arr1= [33, 4, 55, 89 ,12]
+print(type(arr1)) #output: <class 'list'>
+
