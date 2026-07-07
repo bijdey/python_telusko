4a32427926c56030cb78fc26683f9fb823611370
vijdey <pandey85880@gmail.com>
2026-07-07 22:23:32 +0530

    made changes

--- intermediate/11array.py
+++ intermediate/11array.py
@@ -8,3 +8,16 @@ from array import *
 arr1= [33, 4, 55, 89 ,12]
 print(type(arr1)) #output: <class 'list'>
 
+arr2= array('i', [33, 4, 55, 89 ,12])
+print(type(arr2)) #output: <class 'array.array'>
+print(arr2) #array('i', [33, 4, 55, 89, 12])
+print(arr2.tolist()) #[33, 4, 55, 89, 12]
+print(arr2.buffer_info())
+
+
+for n in arr1:
+    print(n)  #this will print the value one by 
+    
+
+
+
